418a7b6a42a7a711a5e426140bc778e4e290e1e6
vuthilinhbeng <vuthilinhbeng@gmail.com>
2022-09-06 14:58:42 +0700

    Linh_new_3

--- dangky.php
+++ dangky.php
@@ -1,6 +1,5 @@
-
 <?php
-$con = mysqli_connect("localhost","root","","blackrouge",3333);
+$con = mysqli_connect("localhost","root","","blackrouge");
 if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   exit();
