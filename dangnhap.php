900214cbacef070b3b3fae15caae4963820ee163
vuthilinhbeng <vuthilinhbeng@gmail.com>
2022-09-06 15:11:36 +0700

    linh_1

--- dangnhap.php
+++ dangnhap.php
@@ -1,6 +1,6 @@
 
 <?php
-$con = mysqli_connect("localhost","root","","blackrouge",3333);
+$con = mysqli_connect("localhost","root","","blackrouge");
 
 if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
